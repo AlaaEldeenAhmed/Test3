0b21520a77bf5ec1db0143c4fe3a532734115ef2
alaa <alaaahmed8383@gmail.com>
2022-01-03 12:04:06 +0400

    Add project files.

---
+++ Test3/Areas/Admin/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Test3.Areas.Admin.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
